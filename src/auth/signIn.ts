c52c648f0903659de97d8a3134b0af792fed2718
TaherSaid <amosiwar@gmail.com>
2022-10-07 10:23:51 +0100

    Create signIn function

---
+++ src/auth/signIn.ts
@@ -0,0 +1,10 @@
+import { Auth } from "aws-amplify";
+
+export async function signIn(username: string, password: string):Promise<boolean> {
+  try {
+    await Auth.signIn(username, password)
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
